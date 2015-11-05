d9b3cf2aec56cbe443c7842300b0f1b6200f9f44
Alexander S. Timmermans <alexander.timmermans@stud.unibas.ch>
2015-11-05 12:42:13 +0100

    Change name in order to follow angular naming conventions, now it is called ramTouchSpin. Heavily modify the spinner directive (it is almost completely rewritten).

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# ngTouchSpin
-AngularJS Implementation of Bootstrap Touchspin
+# ramTouchSpin
+
+AngularJS implementation of Bootstrap Touchspin.
+
+Note: this project started as a fork of [jkuri.ngTouchSpin](https://github.com/jkuri/ngTouchSpin). It has been heavily modified and also renamed in order to follow the angular naming convetions.
+This implementation overcomes some issues of the original version, for example does arrow keys not block normal key input. The scope variable is a number and not a string.
+
+Usage:
+
+```html
+<input type="text" model="testValue" ram-touch-spin>
+```
+
+Various options are availbe as decribed in the following table:
+
+| Attribute           | Default        | Description |
+|:------------------- |:--------------:|:----------------------------------------------------------------------------------------------------------------------------------------------- |
+| model               | undefined      | The scope variable to be displayed. This option is required!   |
+| vertical            | true           | If true show up and down buttons on the right hand side of the input field show plus (+) and (-) buttons on the right and left side otherwise   |
+| step                | 1              | Incremental/decremental step on up/down change. |
+| initval             | undefined      | If the scope variable already exists this option is ignored otherwise the value is used to initialy set the scope variable.
+| min                 | undefined      | Minimum value. |
+| max                 | undefined      | Maximum value. |
+| decimals            | 0              | Number of decimal points. |
+| step-interval       | 100            | Refresh rate of the spinner in milliseconds. |
+| step-interval-delay | 500            | Time in milliseconds before the spinner starts to spin. |
+| prefix              | ""             | Text before the input. |
+| postfix             | ""             | Text after the input. |
+| decimal-sep         | "."            | Decimal separation character |
+| with-key            | true           | Use up and down arrow key to increas or decrease the number |
+
+
+
+
+
+
+
---
+++ src/js/ramTouchSpin.js
@@ -0,0 +1,191 @@
+angular.module('ram.touchspin', [])
+
+.directive('ramTouchSpin', ['$timeout', '$interval', '$document', function ($timeout, $interval, $document) {
+    'use strict';
+
+    var keyCodes = {
+        left: 37,
+        right: 39,
+        up: 38,
+        down: 40
+    };
+
+    var setScopeValues = function (scope, attrs) {
+        scope.min = parseFloat(Number(attrs.min)) || undefined;
+        scope.max = parseFloat(Number(attrs.max)) || undefined;
+        scope.step = attrs.step || 1;
+        scope.prefix = attrs.prefix || undefined;
+        scope.postfix = attrs.postfix || undefined;
+        scope.decimals = attrs.decimals || 0;
+        scope.stepInterval = attrs.stepInterval || 100;
+        scope.stepIntervalDelay = attrs.stepIntervalDelay || 500;
+        scope.initval = attrs.initval || '';
+        scope.model = scope.model || scope.initval;
+        scope.decimalSep = attrs.decimalSep || '.';
+        if (attrs.withKey === "false") {
+            scope.withKey = false;
+        } else {
+            scope.withKey = true;
+        }
+        if (attrs.vertical === "false") {
+            scope.verticalButtons = false;
+        } else {
+            scope.verticalButtons = true;
+        }
+        if (scope.decimalSep === ".") {
+            scope.regex = /^-?(?:\d+|\d*\.(\d+)?)$/i;
+        } else if (scope.decimalSep === ",") {
+            scope.regex = /^-?(?:\d+|\d*\,(\d+)?)$/i;
+        } else {
+            //any decimal separator
+            scope.regex = new RegExp("^-?(?:\\d+|\\d*" + scope.decimalSep + "(\\d+)?)$");
+        }
+    };
+
+    var toFloat = function (value, decimalSep) {
+        if (decimalSep !== "." && (typeof value === "string" || value instanceof String)) {
+            value = value.replace(decimalSep, ".");
+        }
+        value = parseFloat(Number(value));
+        return value;
+    }
+
+    var toString = function (value, decimalSep) {
+        if(value === null || value === undefined){
+            return "";
+        }
+        if( typeof value === 'number'){
+            value = value.toString();
+        }
+        value = value.replace('.', decimalSep);
+        return value;
+    }
+
+
+    return {
+        restrict: 'EA',
+        scope: {
+            model: "="
+        },
+        replace: true,
+        link: function (scope, element, attrs) {
+            var timeout, timer, clickStart;
+            scope.focused = false;
+
+            setScopeValues(scope, attrs);
+
+            //val is our copy in string representation
+            scope.val = toString(scope.model, scope.decimalSep);
+
+            scope.$watch('model', function (newVal, oldVal) {
+                if (newVal === oldVal) return;
+                scope.val = toString(newVal, scope.decimalSep);
+            }, false);
+
+            scope.updateValue = function () {
+                if (scope.val !== undefined) {
+                    scope.model = toFloat(scope.val, scope.decimalSep);
+                }
+            }
+
+            scope.increment = function () {
+                var value = parseFloat(parseFloat(Number(scope.model)) + parseFloat(scope.step)).toFixed(scope.decimals);
+                if (scope.max != undefined && value > scope.max) return;
+                scope.model = value;
+            };
+
+            scope.decrement = function () {
+                var value = parseFloat(parseFloat(Number(scope.model)) - parseFloat(scope.step)).toFixed(scope.decimals);
+                if (scope.max != undefined && value < scope.min) {
+                    value = parseFloat(scope.min).toFixed(scope.decimals);
+                    scope.model = value;
+                    return;
+                }
+                scope.model = value;
+            };
+
+            scope.startSpinUp = function () {
+                scope.increment();
+
+                clickStart = Date.now();
+                scope.stopSpin();
+
+                $timeout(function () {
+                    timer = $interval(function () {
+                        scope.increment();
+                    }, scope.stepInterval);
+                }, scope.stepIntervalDelay);
+            };
+
+            scope.startSpinDown = function () {
+                scope.decrement();
+
+                clickStart = Date.now();
+                scope.stopSpin();
+
+                timeout = $timeout(function () {
+                    timer = $interval(function () {
+                        scope.decrement();
+                    }, scope.stepInterval);
+                }, scope.stepIntervalDelay);
+            };
+
+            scope.stopSpin = function () {
+                if (Date.now() - clickStart > scope.stepIntervalDelay) {
+                    $timeout.cancel(timeout);
+                    $interval.cancel(timer);
+                } else {
+                    $timeout(function () {
+                        $timeout.cancel(timeout);
+                        $interval.cancel(timer);
+                    }, scope.stepIntervalDelay);
+                }
+            };
+
+            scope.focus = function () {
+                scope.focused = true;
+            }
+
+            scope.blur = function () {
+                scope.focused = false;
+            }
+
+            var $body = $document.find('body');
+            $body.bind('keydown', function (event) {
+                if (!scope.withKey || !scope.focused) {
+                    return;
+                }
+                event = event || window.event;
+                var which = event.which;
+                if (which === keyCodes.up) {
+                    scope.increment();
+                    event.preventDefault();
+                    scope.$apply();
+                } else if (which === keyCodes.down) {
+                    scope.decrement();
+                    event.preventDefault();
+                    scope.$apply();
+                }
+            });
+        },
+
+        template:
+		'<div class="input-group bootstrap-touchspin">' +
+		'  <span class="input-group-btn" ng-if="!verticalButtons">' +
+		'    <button class="btn btn-default bootstrap-touchspin-down" ng-mousedown="startSpinDown()" ng-mouseup="stopSpin()"><i class="fa fa-minus"></i></button>' +
+		'  </span>' +
+		'  <span class="input-group-addon bootstrap-touchspin-prefix" ng-show="prefix" ng-bind="prefix"></span>' +
+		'  <input type="text" ng-model="val" ng-pattern="regex" class="form-control" ng-change="updateValue()" ng-blur="blur()" ng-focus="focus()">' +
+		'  <span class="input-group-addon bootstrap-touchspin-postfix" ng-show="postfix" ng-bind="postfix"></span>' +
+        '  <span class="input-group-btn" ng-if="!verticalButtons">' +
+		'    <button class="btn btn-default bootstrap-touchspin-down" ng-mousedown="startSpinUp()" ng-mouseup="stopSpin()"><i class="fa fa-plus"></i></button>' +
+		'  </span>' +
+        '  <span class="input-group-btn-vertical" ng-if="verticalButtons">' +
+        '      <button class="btn btn-default bootstrap-touchspin-up" type="button" ng-mousedown="startSpinUp()" ng-mouseup="stopSpin()"><i class="glyphicon glyphicon-chevron-up"></i></button>' +
+        '      <button class="btn btn-default bootstrap-touchspin-down" type="button"ng-mousedown="startSpinDown()" ng-mouseup="stopSpin()"><i class="glyphicon glyphicon-chevron-down"></i></button>' +
+        '  </span>' +
+		'</div>'
+
+    };
+
+}]);
